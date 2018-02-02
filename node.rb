702ace93191c54c89471a6986a80f44e676f2612
IDK <liketoaccess@gmail.com>
2018-02-02 15:58:30 -0600

    Bug Fixes

--- node.rb
+++ node.rb
@@ -1,9 +1,9 @@
 #! usr/bin/env ruby
 
 #Chat App. Using comments regularly for the first time. Tryin to get into the habit. #This file is the server. To join it, run Towel.rb
-#!/usr/bin/env ruby -w
+
 require "socket"
-class TelnetTowel
+class Server
   def initialize(port, ip)
     @server = TCPServer.open(ip, port)
     @connections = Hash.new
@@ -17,32 +17,32 @@ class TelnetTowel
 
   def run
     loop {
-      Thread.start(@server.accept) do |client|
-        nickName = client.gets.chomp.to_sym
-        @connections[:clients].each do |otherName, otherClient|
-          if nickName == otherName or client == otherClient
-            client.puts "This Username is already being used."
+      Thread.start(@server.accept) do | client |
+        nick_name = client.gets.chomp.to_sym
+        @connections[:clients].each do |other_name, other_client|
+          if nick_name == other_name || client == other_client
+            client.puts "This username already exist"
             Thread.kill self
           end
         end
-        puts "#{nickName} #{client}"
-        @connections[:clients][nickName] = client
-        client.puts ""
-        listenUserMessages(nickName, client)
+        puts "#{nick_name} #{client}"
+        @connections[:clients][nick_name] = client
+        client.puts “Welcome. Thank you for joinin the Towel”
+        listen_user_messages( nick_name, client )
       end
     }.join
   end
 
-  def listenUserMessages(username, client)
+  def listen_user_messages(username, client)
     loop {
       msg = client.gets.chomp
-      @connections[:clients].each do |otherName, otherClient|
-        unless otherName == username
-          otherClient.puts "#{username.to_s}: #{msg}"
+      @connections[:clients].each do |other_name, other_client|
+        unless other_name == username
+          other_client.puts "#{username.to_s}: #{msg}"
         end
       end
     }
   end
 end
 
-TelnetTowel.new(3000, "localhost")
+Server.new(3000, "localhost")
